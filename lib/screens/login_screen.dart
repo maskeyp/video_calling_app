fea1d62b0b662a27692189157d4c805422edf975
maskey <pawan@actnepal.com.np>
2022-08-28 16:36:24 +0545

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:video_calling_app/resources/auth_methods.dart';
+
+import '../widgets/button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  
+  final AuthMethods _authMethods = AuthMethods();
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Start or Join Meeetings',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 38.0),
+              child: Image.asset('assets/images/onboarding.jpg'),
+            ),
+            CustomButton(
+              text: 'Google Sign In',
+              onPressed: () async {
+                bool res = await _authMethods.signInWithGoogle(context);
+                if (res) {
+                  Navigator.pushNamed(context, '/home');
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
